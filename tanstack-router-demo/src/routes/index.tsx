20763e109f93adac0545fc00dfdcd4901299635c
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2026-03-16 23:39:27 +0900

    修正

--- tanstack-router-demo/src/routes/index.tsx
+++ tanstack-router-demo/src/routes/index.tsx
@@ -7,7 +7,7 @@ export const Route = createFileRoute("/")({
 function HomePage() {
   return (
     <div>
-      <h1>🎉 TanStack Router デモへようこそ！</h1>
+      <h1>TanStack Router デモへようこそ！</h1>
       <p>このサイトでTanStack Routerの基本的な使い方を学びましょう。</p>
     </div>
   );
